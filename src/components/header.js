ebd38d9098de7e24afca7a88c34bb4403e5b79d1
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2023-02-23 13:58:15 +0530

    final

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom/client";
+
+
+export default function Nav(){
+
+    return(
+        <nav className="navbar">
+            <img className="nav-img" alt="smirkface" src="images/smirkface.png" />
+            <h2>MEME GENERATOR</h2>
+        </nav>
+    )
+}
